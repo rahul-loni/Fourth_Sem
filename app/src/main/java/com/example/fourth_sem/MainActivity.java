59f079c8534f574e26903ee498bc6cd66db4b3b3
Rahul Kumar <rahul@gmail.com>
2024-06-28 07:55:23 +0545

    click method

--- app/src/main/java/com/example/fourth_sem/MainActivity.java
+++ app/src/main/java/com/example/fourth_sem/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.fourth_sem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn1=findViewById(R.id.btn);
+        txt1=findViewById(R.id.txt);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent btn1intent=new Intent(MainActivity.this,LoginPage.class);
+                startActivity(btn1intent);
+                Toast.makeText(MainActivity.this, "Click btn1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        txt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent txt1intent=new Intent(MainActivity.this,Signup.class);
+                startActivity(txt1intent);
+                Toast.makeText(MainActivity.this, "Click btn1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
